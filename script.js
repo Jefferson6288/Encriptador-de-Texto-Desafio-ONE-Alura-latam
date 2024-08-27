5d78dda4b1aa16086103b4e79ca4b0777b4af5a7
Jefferson Largo <Jefferson628@gmail.com>
2024-08-26 22:37:20 -0500

    ingreso de las llaves de encriptacion

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-Const: TextArea = Document.querySelector(".text-area")
+const textArea = document.querySelector(".text-area")
+const Mensaje =document.querySelector("texto-resultado")
+
+let matrizDecodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]]
