c4bbf0d8e8eda501655660d4480948b881333bb9
Jefferson Largo <Jefferson628@gmail.com>
2024-08-26 23:17:30 -0500

    modificaciones al script

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 const textArea = document.querySelector(".text-area")
-const Mensaje =document.querySelector("texto-resultado")
+const Mensaje = document.querySelector(".textoresultado")
 
-let matrizDecodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]]
+function BtnEncriptar(){
+    const TextoEncriptado = encriptar(textArea.value)
+    mensaje.value = TextoEncriptado
+    textArea.value = "";
+}
+
+function encriptar (StringEncriptada){
+    let matrizDecodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]]};
+    StringEncriptada = StringEncriptada.tolowercase
+
+for(let i = 0; i < matrizDecodigo.length;i++){
+    if(StringEncriptada.includes(matrizDecodigo[i][0])){
+        StringEncriptada = StringEncriptada.replaceAll(matrizDecodigo[i][0], matrizDecodigo[i][1])
+    }}
